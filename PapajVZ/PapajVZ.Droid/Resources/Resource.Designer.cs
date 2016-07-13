e68133aa83ec33e36cbfdb2472f6de8df8ee382f
Antonio Lopac <lopac.antonio@outlook.com>
2016-07-13 23:41:52 +0200

    Gif loading add

--- PapajVZ/PapajVZ.Droid/Resources/Resource.Designer.cs
+++ PapajVZ/PapajVZ.Droid/Resources/Resource.Designer.cs
@@ -4072,64 +4072,67 @@ namespace PapajVZ.Droid
 			public const int Icon = 2130837654;
 			
 			// aapt resource value: 0x7f020097
-			public const int logo_512 = 2130837655;
+			public const int infinity = 2130837655;
 			
 			// aapt resource value: 0x7f020098
-			public const int mr_dialog_material_background_dark = 2130837656;
+			public const int logo_512 = 2130837656;
 			
 			// aapt resource value: 0x7f020099
-			public const int mr_dialog_material_background_light = 2130837657;
+			public const int mr_dialog_material_background_dark = 2130837657;
 			
 			// aapt resource value: 0x7f02009a
-			public const int mr_ic_audiotrack_light = 2130837658;
+			public const int mr_dialog_material_background_light = 2130837658;
 			
 			// aapt resource value: 0x7f02009b
-			public const int mr_ic_cast_dark = 2130837659;
+			public const int mr_ic_audiotrack_light = 2130837659;
 			
 			// aapt resource value: 0x7f02009c
-			public const int mr_ic_cast_light = 2130837660;
+			public const int mr_ic_cast_dark = 2130837660;
 			
 			// aapt resource value: 0x7f02009d
-			public const int mr_ic_close_dark = 2130837661;
+			public const int mr_ic_cast_light = 2130837661;
 			
 			// aapt resource value: 0x7f02009e
-			public const int mr_ic_close_light = 2130837662;
+			public const int mr_ic_close_dark = 2130837662;
 			
 			// aapt resource value: 0x7f02009f
-			public const int mr_ic_media_route_connecting_mono_dark = 2130837663;
+			public const int mr_ic_close_light = 2130837663;
 			
 			// aapt resource value: 0x7f0200a0
-			public const int mr_ic_media_route_connecting_mono_light = 2130837664;
+			public const int mr_ic_media_route_connecting_mono_dark = 2130837664;
 			
 			// aapt resource value: 0x7f0200a1
-			public const int mr_ic_media_route_mono_dark = 2130837665;
+			public const int mr_ic_media_route_connecting_mono_light = 2130837665;
 			
 			// aapt resource value: 0x7f0200a2
-			public const int mr_ic_media_route_mono_light = 2130837666;
+			public const int mr_ic_media_route_mono_dark = 2130837666;
 			
 			// aapt resource value: 0x7f0200a3
-			public const int mr_ic_pause_dark = 2130837667;
+			public const int mr_ic_media_route_mono_light = 2130837667;
 			
 			// aapt resource value: 0x7f0200a4
-			public const int mr_ic_pause_light = 2130837668;
+			public const int mr_ic_pause_dark = 2130837668;
 			
 			// aapt resource value: 0x7f0200a5
-			public const int mr_ic_play_dark = 2130837669;
+			public const int mr_ic_pause_light = 2130837669;
 			
 			// aapt resource value: 0x7f0200a6
-			public const int mr_ic_play_light = 2130837670;
+			public const int mr_ic_play_dark = 2130837670;
 			
 			// aapt resource value: 0x7f0200a7
-			public const int n_favorite = 2130837671;
-			
-			// aapt resource value: 0x7f0200aa
-			public const int notification_template_icon_bg = 2130837674;
+			public const int mr_ic_play_light = 2130837671;
 			
 			// aapt resource value: 0x7f0200a8
-			public const int settings = 2130837672;
+			public const int n_favorite = 2130837672;
+			
+			// aapt resource value: 0x7f0200ab
+			public const int notification_template_icon_bg = 2130837675;
 			
 			// aapt resource value: 0x7f0200a9
-			public const int stat = 2130837673;
+			public const int settings = 2130837673;
+			
+			// aapt resource value: 0x7f0200aa
+			public const int stat = 2130837674;
 			
 			static Drawable()
 			{
@@ -4327,8 +4330,8 @@ namespace PapajVZ.Droid
 			// aapt resource value: 0x7f070041
 			public const int image = 2131165249;
 			
-			// aapt resource value: 0x7f07009d
-			public const int indicator = 2131165341;
+			// aapt resource value: 0x7f07009e
+			public const int indicator = 2131165342;
 			
 			// aapt resource value: 0x7f070098
 			public const int info = 2131165336;
@@ -4350,6 +4353,9 @@ namespace PapajVZ.Droid
 			
 			// aapt resource value: 0x7f070043
 			public const int list_item = 2131165251;
+			
+			// aapt resource value: 0x7f07009c
+			public const int loadingView = 2131165340;
 			
 			// aapt resource value: 0x7f070090
 			public const int media_actions = 2131165328;
@@ -4447,8 +4453,8 @@ namespace PapajVZ.Droid
 			// aapt resource value: 0x7f07000c
 			public const int normal = 2131165196;
 			
-			// aapt resource value: 0x7f07009c
-			public const int pager = 2131165340;
+			// aapt resource value: 0x7f07009d
+			public const int pager = 2131165341;
 			
 			// aapt resource value: 0x7f070028
 			public const int parallax = 2131165224;
